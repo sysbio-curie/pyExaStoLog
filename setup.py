b07c9a4d9a8a3435be98132e9c3427172470fc96
Vincent Noël <contact@vincent-noel.fr>
2020-07-08 14:58:28 +0200

    Added scikit-umfpack dependency

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='exastolog',
     author="Vincent Noel",
     author_email="contact@vincent-noel.fr",
     description="A library for the exact calculation of stationary states of MaBoSS models",
-	install_requires = ['numpy', 'scipy', 'boolean.py', 'networkx'],
+	install_requires = ['numpy', 'scipy', 'boolean.py', 'networkx', 'scikit-umfpack'],
 	packages=['exastolog'],
 )
