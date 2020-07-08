97e0590d20d64e2e8bb283b367d2d6e66d2d5a8e
Vincent Noël <contact@vincent-noel.fr>
2020-07-08 15:07:48 +0200

    Fixed tests

--- exastolog/test/test_toy3.py
+++ exastolog/test/test_toy3.py
@@ -10,7 +10,7 @@ class TestToy3(TestCase):
         model = Model(join(dirname(__file__), "../../notebooks/model_files/toy3.bnet"))
         simulation = Simulation(model, ['A','B'], [0, 0])
         
-        self.assertEqual(simulation.solution.stat_sol[0, 0], 0.25)
-        self.assertEqual(simulation.solution.stat_sol[1, 0], 0.25)
-        self.assertEqual(simulation.solution.stat_sol[2, 0], 0.25)
-        self.assertEqual(simulation.solution.stat_sol[3, 0], 0.25)
+        self.assertAlmostEqual(simulation.solution.stat_sol[0, 0], 0.25)
+        self.assertAlmostEqual(simulation.solution.stat_sol[1, 0], 0.25)
+        self.assertAlmostEqual(simulation.solution.stat_sol[2, 0], 0.25)
+        self.assertAlmostEqual(simulation.solution.stat_sol[3, 0], 0.25)
