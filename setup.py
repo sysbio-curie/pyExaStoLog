a8474823c1b5ef1642339b022675e0d60e340073
Vincent Noël <contact@vincent-noel.fr>
2020-07-08 20:20:00 +0200

    Added pandas requirement

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='exastolog',
-    version="1.0.0a4",
+    version="1.0.0a5",
     author="Vincent Noel",
     author_email="contact@vincent-noel.fr",
     description="A library for the exact calculation of stationary states of MaBoSS models",
-	install_requires = ['numpy', 'scipy', 'boolean.py', 'networkx', 'scikit-umfpack'],
+	install_requires = ['numpy', 'scipy', 'boolean.py', 'networkx', 'scikit-umfpack', 'pandas'],
 	packages=['exastolog'],
 )
